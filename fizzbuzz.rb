0b0af00a8cb7d3fe5ba6135bbb0a701721475956
Bosco So <bosco@sfruby.info>
2014-07-21 01:25:26 -0700

    hiding test suite inside a conditional so that file can be required - preparing for inclusion into Sinatra

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
-require 'minitest/autorun'
+#
+# fizzbuzz.rb
+#
 
-class Numeric
-  def multiple_of?(num)
-    0 == self % num
-  end
-end
-
+# Return array that is the "fizz-buzz" of the limit argument.
+#
 def fizzbuzz(limit)
   (1..limit).inject([]) do |memo, count|
     if count.multiple_of?(3) && count.multiple_of?(5)
@@ -20,33 +18,47 @@ def fizzbuzz(limit)
   end
 end
 
-class FizzBuzzTest < MiniTest::Unit::TestCase
-
-  def test_0_case
-    assert_equal([], fizzbuzz(0))
+# Open Numeric class to add multiple_of? method.
+#
+class Numeric
+  def multiple_of?(num)
+    0 == self % num
   end
+end
 
-  def test_1_case
-    assert_equal [1], fizzbuzz(1)
-  end
+# Run test suite on direct invocation
+#
+if __FILE__ == $0
+  require 'minitest/autorun'
 
-  def test_3
-    assert_equal [1, 2, 'fizz'], fizzbuzz(3)
-  end
+  class FizzBuzzTest < MiniTest::Unit::TestCase
 
-  def test_4
-    assert_equal [1, 2, 'fizz', 4], fizzbuzz(4)
-  end
+    def test_0_case
+      assert_equal([], fizzbuzz(0))
+    end
 
-  def test_5
-    assert_equal [1, 2, 'fizz', 4, 'buzz'], fizzbuzz(5)
-  end
+    def test_1_case
+      assert_equal [1], fizzbuzz(1)
+    end
 
-  def test_6
-    assert_equal [1, 2, 'fizz', 4, 'buzz', 'fizz'], fizzbuzz(6)
-  end
+    def test_3
+      assert_equal [1, 2, 'fizz'], fizzbuzz(3)
+    end
 
-  def test_15
-    assert_equal [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizz-buzz'], fizzbuzz(15)
+    def test_4
+      assert_equal [1, 2, 'fizz', 4], fizzbuzz(4)
+    end
+
+    def test_5
+      assert_equal [1, 2, 'fizz', 4, 'buzz'], fizzbuzz(5)
+    end
+
+    def test_6
+      assert_equal [1, 2, 'fizz', 4, 'buzz', 'fizz'], fizzbuzz(6)
+    end
+
+    def test_15
+      assert_equal [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizz-buzz'], fizzbuzz(15)
+    end
   end
 end
